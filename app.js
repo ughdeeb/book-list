7629f9f355d379b0552f8788b795438e5a006d58
ughdeeb <adeebsyed725@gmail.com>
2021-04-19 17:28:21 -0400

    validation functionality added

---
+++ app.js
@@ -0,0 +1,67 @@
+//BOOK CONSTRUCTOR
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+//UI CONSTRUCTOR
+
+function UI() {}
+
+UI.prototype.addBookToList = function (book) {
+  const list = document.getElementById("book-list");
+  const row = document.createElement("tr");
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href ="#" class="delete">X</a></td>`;
+
+  list.appendChild(row);
+};
+
+// Show Alert
+
+UI.prototype.showAlert = function (message, className) {
+  const div = document.createElement("div");
+  div.className = `alert ${className}`;
+  div.appendChild(document.createTextNode(message));
+  const container = document.querySelector(".container");
+  const form = document.querySelector("#book-form");
+  container.insertBefore(div, form);
+  setTimeout(function () {
+    document.querySelector(".alert").remove();
+  }, 3000);
+};
+
+UI.prototype.clearFields = function () {
+  document.getElementById("title").value = "";
+  document.getElementById("title").value = "";
+  document.getElementById("title").value = "";
+};
+
+//EVENT LISTENERS
+document.getElementById("book-form").addEventListener("submit", function (e) {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const isbn = document.getElementById("isbn").value;
+
+  //INSTANTIATE A BOOK FROM THE CONSTRUCTOR ABOVE
+  const book = new Book(title, author, isbn);
+
+  //INSTANTIATE A UI OBJECT
+  const ui = new UI();
+
+  // VALIDATE IF A BOOK IS ENTERED
+
+  if (title === "" || author === "" || isbn === "") {
+    //ERROR ALERT
+    ui.showAlert("Please fill in all fields", "error");
+  } else {
+    ui.addBookToList(book);
+    ui.showAlert("Book added!", "success");
+    ui.clearFields();
+  }
+
+  e.preventDefault();
+});
